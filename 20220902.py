01da6284a3bcc1f9e51ead60b72e9e71d5d6e188
PARKUNGJUNG <93420579+PARKUNGJUNG@users.noreply.github.com>
2022-09-03 00:31:12 +0900

    Add files via upload

---
+++ 20220902.py
@@ -0,0 +1,45 @@
+print("pencil")
+print("eraser")
+print("pen")
+
+print(1 + 2 + 3)
+
+print((4 + 8) / 2)
+
+print((-5 -1)*3) # -5 + -1 임. -5 - 1 = -4 가 아님.
+
+'''
+You need to calculate the points earned by a soccer team.
+The team won 18 games and ended 7 games as a draw.
+A win brings 3 points, while a draw brings 1.'''
+
+print((18*3) + (7*1))
+
+print( (8/4)/2 )
+
+print (100)
+print ("박웅정")
+
+print (4.13 * 7)
+
+
+'''
+print (divmod(50,8))
+
+
+Using a single slash to divide numbers always produces a decimal (or float, as it is called in programming)
+, even if the result is a whole number.
+
+
+print((-5 -1)*3) # -5 + -1 임. -5 - 1 = -4 가 아님.
+
+
+When you add an integer to a float, the result is always a float.
+
+
+Integers are whole numbers, such as 8 and 42.
+Floats are numbers with a decimal point, like 3.14.
+Strings are text enclosed in single or double quotes.
+
+Dividing with a slash produces a float, even if you divide whole numbers.
+'''
