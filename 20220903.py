fbb6f50f991992990c764608876b1f72ec828159
PARKUNGJUNG <93420579+PARKUNGJUNG@users.noreply.github.com>
2022-09-04 20:20:57 +0900

    Add files via upload

---
+++ 20220903.py
@@ -0,0 +1,46 @@
+# Two asterisks ** are used to raise a number to the power of another, 
+# which is called exponentiation.
+
+
+print(2**5)
+
+
+# You can chain exponentiations together.
+# In other words, you can raise a number to a power, 
+# which can also be raised to a power.
+
+print(2**3**2)
+# = 2⁹
+
+print(9 ** (1 / 2))
+
+print(8 ** (1 / 3))
+
+# //를 사용해 output 하는 경우 몫만 나옴.
+print(10 // 4)
+
+# %를 사용해 output 하는 경우 나머지만 나옴.
+print(20 % 6)
+
+#Write code to output 4 raised to the 5th power.
+print (4 ** 5)
+
+'''
+You need to calculate the flight time of an upcoming trip. You are flying from LA to Sydney,
+ covering a distance of 7425 miles,
+ the plane flies at an average speed of 550 miles an hour.
+
+Calculate and output the total flight time in hours.
+
+Hint
+The result should be a float.
+Use the print statement to output the result.
+'''
+print(7425 / 550)
+#float도 나와야 하기 때문에 /를 써야함.
+
+print("42")
+#이 위의 42는 정수가 아닌 문자열로 취급됨.
+
+# ' , "를 문자열로 취급하고 싶을 때, \를 써주면 된다.
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
